e4b253f23d6f1c1c5b35e9a9c20f399d1f257e00
thriwax <91975849+thriwax@users.noreply.github.com>
2023-12-13 13:16:08 +0300

    add some projects

--- src/app/utils/projectsData.js
+++ src/app/utils/projectsData.js
@@ -9,14 +9,22 @@ const projectsData = [
     },
     {
         id: 2,
-        title: `Speech Center Website`,
-        description: `#HTML #CSS #JS`,
-        image: '/images/projects/2.png',
+        title: `LVG Real Estate Agency`,
+        description: `#WORDPRESS #ELEMENTOR`,
+        image: '/images/projects/11.png',
         tag: ['All', 'Web + Design'],
-        link: 'https://mirpoznanija.ru/'
+        link: 'https://lvgrealestatedubai.com/'
     },
     {
         id: 3,
+        title: `Fiable`,
+        description: `#WORDPRESS #ELEMENTOR #WOOCOMMERCE`,
+        image: '/images/projects/10.png',
+        tag: ['All', 'Web + Design'],
+        link: 'https://fiableluxury.com/'
+    },
+    {
+        id: 4,
         title: `Equipment Rental Website`,
         description: `#HTML #SCSS #JS`,
         image: '/images/projects/3.png',
@@ -24,47 +32,15 @@ const projectsData = [
         link: 'https://xn----7sbbrpejcjkehcibuqsd5d.xn--p1ai/'
     },
     {
-        id: 4,
-        title: `Crypto Wallet Landing Page`,
-        description: `#HTML #SCSS #JS`,
-        image: '/images/projects/4.png',
-        tag: ['All', 'Web'],
-        link: 'https://thriwax.github.io/landing-page-crypto-wallet/'
-    },
-    {
         id: 5,
-        title: `NFT Marketplace Landing Page`,
-        description: `#HTML #SCSS #JS`,
-        image: '/images/projects/5.png',
-        tag: ['All', 'Web'],
-        link: 'https://thriwax.github.io/landing-page-nft-markteplace/'
+        title: `Speech Center Website`,
+        description: `#HTML #CSS #JS`,
+        image: '/images/projects/2.png',
+        tag: ['All', 'Web + Design'],
+        link: 'https://mirpoznanija.ru/'
     },
     {
         id: 6,
-        title: `Portfolio Website`,
-        description: `#NEXT.JS #TAILWIND`,
-        image: '/images/projects/6.png',
-        tag: ['All', 'Web + Design'],
-        link: '/'
-    },
-    {
-        id: 7,
-        title: `Instabot Landing Page`,
-        description: `#HTML #CSS`,
-        image: '/images/projects/7.png',
-        tag: ['All', 'Web'],
-        link: 'https://thriwax.github.io/landing-page-instabot/'
-    },
-    {
-        id: 8,
-        title: `DeKrupe Law Pages`,
-        description: `#WORDPRESS #WPBAKERY`,
-        image: '/images/projects/8.png',
-        tag: ['All', 'Web'],
-        link: 'https://dekrupelaw.ca/divorce-lawyers-toronto-gta/'
-    },
-    {
-        id: 9,
         title: `Right Bins Pages`,
         description: `#WORDPRESS #WPBAKERY`,
         image: '/images/projects/9.png',
@@ -72,12 +48,44 @@ const projectsData = [
         link: 'https://rightbins.ca/bobcat-track-loader-rental/'
     },
     {
+        id: 7,
+        title: `DeKrupe Law Pages`,
+        description: `#WORDPRESS #WPBAKERY`,
+        image: '/images/projects/8.png',
+        tag: ['All', 'Web'],
+        link: 'https://dekrupelaw.ca/divorce-lawyers-toronto-gta/'
+    },
+    {
+        id: 8,
+        title: `Crypto Wallet Landing Page`,
+        description: `#HTML #SCSS #JS`,
+        image: '/images/projects/4.png',
+        tag: ['All', 'Web'],
+        link: 'https://thriwax.github.io/landing-page-crypto-wallet/'
+    },
+    {
+        id: 9,
+        title: `NFT Marketplace Landing Page`,
+        description: `#HTML #SCSS #JS`,
+        image: '/images/projects/5.png',
+        tag: ['All', 'Web'],
+        link: 'https://thriwax.github.io/landing-page-nft-markteplace/'
+    },
+    {
         id: 10,
-        title: `Fiable`,
-        description: `#WORDPRESS #ELEMENTOR #WOOCOMMERCE`,
-        image: '/images/projects/10.png',
+        title: `Portfolio Website`,
+        description: `#NEXT.JS #TAILWIND`,
+        image: '/images/projects/6.png',
         tag: ['All', 'Web + Design'],
-        link: 'https://fiableluxury.com/'
+        link: '/'
+    },
+    {
+        id: 11,
+        title: `Instabot Landing Page`,
+        description: `#HTML #CSS`,
+        image: '/images/projects/7.png',
+        tag: ['All', 'Web'],
+        link: 'https://thriwax.github.io/landing-page-instabot/'
     }
 ];
 
