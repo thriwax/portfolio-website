692329f99a327ea31e33a069af5e8ebf46a6cffc
Fedor Tatarintsev <91975849+thriwax@users.noreply.github.com>
2024-11-07 13:32:24 +0300

    Update [slug].jsx

--- src/app/projects/[slug].jsx
+++ src/app/projects/[slug].jsx
@@ -3,25 +3,28 @@ import { useRouter } from 'next/router';
 import projects from '../utils/projectsData';
 
 const ProjectPage = () => {
+  const [project, setProject] = useState(null);
+  const { query } = useRouter();
 
-    const [project, setProject] = useState()
-    
-    const {query} = useRouter()
+  useEffect(() => {
+    if (query.slug) {
+      const foundProject = projects.find((p) => p.id === query.slug);
+      setProject(foundProject);
+    }
+  }, [query.slug]); // Указываем query.slug как зависимость
 
-    useEffect(() => {
-        
-        setMix(projects.find((p) => {
-            return p.id === query.slug}))
+  return (
+    <>
+      {project ? (
+        <div>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+};
 
-    }, [projects, setProject, query])
-    
-    
-    return (
-      <>
-        <div>{project?.title}</div>
-      </>
-    );
-  };
-
-  
-  export default ProjectPage;
+export default ProjectPage;
